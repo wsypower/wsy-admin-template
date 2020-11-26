5987e442a704b4a42bb1a652d3723a52ad036eee
weiyafei <2553241022@qq.com>
2020-11-26 18:11:21 +0800

    调整plugin 结构

--- src/api/tools.js
+++ src/api/tools.js
@@ -24,10 +24,7 @@ export function parse (jsonString = '{}', defaultValue = {}) {
  * @param {Number} code 状态码
  */
 export function response (data = {}, msg = '', code = 0) {
-  return [
-    200,
-    { code, msg, data }
-  ]
+  return [200, { code, msg, data }]
 }
 
 /**
