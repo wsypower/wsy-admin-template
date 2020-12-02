ec7b524e870218d1c2f8eefd7623133ea2046618
weiyafei <2553241022@qq.com>
2020-12-02 21:27:54 +0800

    📦 NEW: 调整router组合逻辑

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,78 +1,32 @@
-import { routerError } from './utils/error'
-import { isMatchSuffix } from './utils/utils'
+/**
+ * TODO 提醒事项
+ * TODO 这里肯定会有很多很多的逻辑，就比如权限自动添加之类的，需要怎么抽离....脑壳大
+ * TODO middleware？EventEmit? 策略模式？
+ */
+
+import {
+  collectRouter,
+  chunkModulesRouter,
+  frameInConcatModules
+} from './utils/utils'
 import { frameOut, errorPage } from './layout-router/sys.router'
 import { frameIn } from './layout-router/frameIn.router'
 
-// ##################################################################### //
-// ################################ 收集路由 ############################### //
-// ##################################################################### //
+console.time('mountRouter')
+// 收集modules下的路由
+const routerModules = collectRouter(require.context('./modules', true, /\.js$/))
 
-/**
- * 收集modules中的路由
- */
-const routerContext = require.context('./modules', true, /\.js$/)
-const frameInRouterModules = routerContext.keys().reduce((children, route) => {
-  // 获取路由标识符
-  const routerKey = isMatchSuffix(route)
-  const routerModules = routerContext(route)
-  // 如果文件为导出为空对象跳过
-  if (!Object.keys(routerModules).length) {
-    // 书写错误提示（阅读源码可跳过）
-    routerError(route)
-    return children
-  }
-  // 兼容 import export 和 require module.export 两种规范
-  children[routerKey] = routerModules.default || routerModules
-  return children
-}, {})
+// 按frameIn内路由name值切分routerModules,需要挂载的放入二维数组第一位，其余放入第二位
+const chunkModulesRouterResult = chunkModulesRouter(routerModules, frameIn)
 
-// ##################################################################### //
-// ################################ 路由分组 ############################### //
-// ##################################################################### //
-/**
- * @description
- * 将Modules收集来的router做切分,frameIn中有同名name的切入第一数组,其余放入第二数组
- * @author wsy
- * @date 2020-12-02  18:45:34
- * @returns {Array} 二维数组
- */
-const frameInRouterModulesKey = Object.keys(frameInRouterModules).reduce(
-  (modulesKey, key) => {
-    const includesFrameRouter = frameIn.findIndex(r => r.name === key)
-    modulesKey[~includesFrameRouter ? 0 : 1].push(key)
-    return modulesKey
-  },
-  [[], []]
-)
-
-// ##################################################################### //
-// ################################ 处理路由 ############################### //
-// ##################################################################### //
-/**
- * @description
- * @author wsy
- * @date 2020-12-02  18:45:40
- * @returns {Array} 将name和key相同的挂载入主路由对应children中，不同的作为主路由混入
- */
-const frameInCollectModules = frameIn.reduce((frameInRouter, router) => {
-  // 只需要匹配 frameInRouterModulesKey[0] 内的router
-  const key = frameInRouterModulesKey[0].findIndex(k => k === router.name)
-  if (~key) {
-    router.children = [
-      ...(Array.isArray(router.children) ? router.children : []),
-      ...frameInRouterModules[frameInRouterModulesKey[0][key]]
-    ]
-    frameInRouter.push(router)
-  }
-  return frameInRouter
-  // reduce 第一个参数放frameInRouterModulesKey[1],混入主框架内
-}, [].concat(...frameInRouterModulesKey[1].map(k => frameInRouterModules[k])))
-
-// ##################################################################### //
-// ################################ 导出路由 ############################### //
-// ##################################################################### //
+// 将name和key值一致的挂载到frameIn内，其余当主路由混入
 // 导出需要显示菜单的
-export const frameInRoutes = frameInCollectModules
+export const frameInRoutes = frameInConcatModules(
+  chunkModulesRouterResult,
+  routerModules,
+  frameIn
+)
+console.timeEnd('mountRouter')
 
 // 重新组织后导出
-export default [...frameInCollectModules, ...frameOut, ...errorPage]
+export default [...frameInRoutes, ...frameOut, ...errorPage]
--- src/router/utils/utils.js
+++ src/router/utils/utils.js
@@ -1,3 +1,5 @@
+import util from '@/libs/util'
+
 /**
  * @description
  * 如果文件名有识别路由标识符，返回值就是识别符
@@ -12,7 +14,7 @@
  * @param {String} 路由文件相对路径
  * @returns {String} key值
  */
-export const isMatchSuffix = routerPath => {
+const isMatchSuffix = routerPath => {
   let routerSuffixKey
   const MatchSuffixRegexp = routerPath.match(/\.([\w-])+\./g)
   if (MatchSuffixRegexp != null) {
@@ -22,4 +24,89 @@ export const isMatchSuffix = routerPath => {
   }
 
   return routerSuffixKey
+}
+
+/**
+ * @description
+ * 收集modules中的路由
+ * @author wsy
+ * @date 2020-12-02  20:57:21
+ */
+const collectRouter = routerContext => {
+  return routerContext.keys().reduce((modules, route) => {
+    // 获取路由标识符
+    const routerKey = isMatchSuffix(route)
+    const routerModules = routerContext(route)
+    // 如果文件为导出为空对象跳过
+    if (!Object.keys(routerModules).length) {
+      // 书写错误提示（阅读源码可跳过）
+      routerError(route)
+      return modules
+    }
+    // 兼容 import export 和 require module.export 两种规范
+    modules[routerKey] = routerModules.default || routerModules
+    return modules
+  }, {})
+}
+
+/**
+ * @description
+ * 将Modules收集来的router做切分,frameIn中有同名name的切入第一数组,其余放入第二数组
+ * @author wsy
+ * @date 2020-12-02  20:56:33
+ * @param {Object} routerModules 收集router数组
+ * @param {Array} frameIn 主框架路由
+ */
+const chunkModulesRouter = (routerModules, frameIn) => {
+  return Object.keys(routerModules).reduce(
+    (modulesKeys, key) => {
+      const FrameRouterIncludes = frameIn.findIndex(r => r.name === key)
+      modulesKeys[~FrameRouterIncludes ? 0 : 1].push(key)
+      return modulesKeys
+    },
+    [[], []]
+  )
+}
+/**
+ * @description
+ * 将name和key相同的挂载入主路由对应children中，不同的作为主路由混入
+ * @author wsy
+ * @date 2020-12-02  20:54:40
+ * @param {Array} modulesKeys 切分后收集数组
+ * @param {Object} routerModules 收集router数组
+ * @param {Array} frameIn 主框架路由
+ */
+const frameInConcatModules = (modulesKeys, routerModules, frameIn) => {
+  return frameIn.reduce((frameInRouter, router) => {
+    // 只需要匹配 modulesKeys[0] 内的router
+    const key = modulesKeys[0].findIndex(k => k === router.name)
+    if (~key) {
+      router.children = [
+        ...(Array.isArray(router.children) ? router.children : []),
+        ...routerModules[modulesKeys[0][key]]
+      ]
+      frameInRouter.push(router)
+    }
+    return frameInRouter
+    // reduce 第一个参数放frameInRouterModulesKey[1],混入主框架内
+  }, [].concat(...modulesKeys[1].map(k => routerModules[k])))
+}
+
+/**
+ * 抛错处理
+ */
+const routerError = route => {
+  if (process.env.NODE_ENV !== 'development') return
+  util.log.capsule('router模块', 'Warning', 'warning')
+  util.log.warning('---------------------------------------')
+  util.log.warning(`router/modules目录:${route.replace('./', '')} 导出对象为空`)
+  util.log.warning('---------------------------------------')
+}
+
+export {
+  isMatchSuffix,
+  routerError,
+  collectRouter,
+  chunkModulesRouter,
+  frameInConcatModules
 }
