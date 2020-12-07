ed7bd8a892d685e2b826786f7d30ee35833486ae
weiyafei <2553241022@qq.com>
2020-12-07 23:30:14 +0800

    📦 NEW: 暂存布局

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -11,11 +11,11 @@ export default {
   watch: {
     '$i18n.locale': 'i18nHandle'
   },
-  created () {
+  created() {
     this.i18nHandle(this.$i18n.locale)
   },
   methods: {
-    i18nHandle (val, oldVal) {
+    i18nHandle(val, oldVal) {
       util.cookies.set('lang', val)
       document.querySelector('html').setAttribute('lang', val)
     }
@@ -25,4 +25,23 @@ export default {
 
 <style lang="scss">
 @import '~@/assets/style/public-class.scss';
+#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  perspective: 1200px;
+}
+.app .app-router-view {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden;
+  backface-visibility: hidden;
+  transform: translate3d(0, 0, 0);
+  transform-style: preserve-3d;
+  visibility: visible;
+}
 </style>
